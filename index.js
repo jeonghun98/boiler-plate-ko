90e4065759461346b666e67cd75d6739612f0367
JeongHun <52409864+jeonghun98@users.noreply.github.com>
2022-07-26 14:27:50 +0900

    11,12 로그인 기능 및 토큰 생성

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const {User} = require("./models/User");
 const config = require('./config/key');
 
@@ -9,6 +10,7 @@ const config = require('./config/key');
 app.use(bodyParser.urlencoded({extended: true}));
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 
 const mongoose = require('mongoose')
@@ -32,6 +34,32 @@ app.post('/register', (req, res) => {
       success : true
     })
   })
+})
+
+app.post('/login', (req, res) => {
+  //요청된 이메일을 데이터 베이스에서 찾는다
+    User.findOne({email:req.body.email}, (err, user) => {
+      if(!user) {
+        return res.json({
+          loginSuccess : false,
+          message : "제공된 이메일에 해당하는 유저가 없습니다."
+        })
+      }
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err,isMatch) => {
+      if(!isMatch)
+      return res.json({loginSuccess : false, message : "비밀번호가 틀림"})
+      //비밀번호까지 맞다면 토큰을 생성하기
+      user.generateToken((err,user) => {
+        if(err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에 ? 쿠키? 로컬스토리지
+        res.cookie("x-auth",user.token)
+        .status(200)
+        .json({loginSuccess : true, userId : user._id})
+      })
+    })
+  });
 })
 
 app.listen(port, () => {
